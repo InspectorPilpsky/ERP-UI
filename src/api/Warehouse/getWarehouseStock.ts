43a6f6250bb3bfb001145dc9d925abef34be3a20
Nikita <66182263+InspectorPilpsky@users.noreply.github.com>
2024-01-28 22:42:59 +0300

    Fix warehouse stock issue (#6)

--- src/api/Warehouse/getWarehouseStock.ts
+++ src/api/Warehouse/getWarehouseStock.ts
@@ -1,7 +1,11 @@
+import { warehouseApi } from "@api/api";
 import { WarehouseStock } from "@domain/Warehouse/WarehouseStock";
-import { PageableWrapper, warehouseApi } from "..";
 import { WarehouseStockDTO } from "./dto/WarehouseStock.dto";
 import { fromWarehouseStockDTOToWarehouseStock } from "./transformers/fromDto";
+
+/*
+
+// Когда- нибудь тут появится панинация
 
 export async function getWarehouseStock(): Promise<PageableWrapper<WarehouseStock[]>> {
 
@@ -10,5 +14,16 @@ export async function getWarehouseStock(): Promise<PageableWrapper<WarehouseStoc
     const content = res.content.map(warehouseStockDto => fromWarehouseStockDTOToWarehouseStock(warehouseStockDto))
     
     return({...res, content});
+
+}
+*/
+
+export async function getWarehouseStock(): Promise<WarehouseStock[]> {
+
+    const res = await warehouseApi.get<WarehouseStockDTO[]>("api/v1/warehouse/stock");
+    
+    const content = res.map(warehouseStockDto => fromWarehouseStockDTOToWarehouseStock(warehouseStockDto))
+    
+    return(content);
 
 }
