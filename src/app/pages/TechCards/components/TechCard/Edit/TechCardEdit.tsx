8f338d0949689f0d26c72884ca6e5cfa83c65ea5
ruabkne <Nikita.BOYARKO@raiffeisen.ru>
2024-03-26 15:43:50 +0300

    Fix techCards saving issue, improve UX

--- src/app/pages/TechCards/components/TechCard/Edit/TechCardEdit.tsx
+++ src/app/pages/TechCards/components/TechCard/Edit/TechCardEdit.tsx
@@ -2,7 +2,8 @@ import { TechCard } from "@domain/TechCard";
 import ComponentSelect from "./components/ComponentSelect/ComponentSelect";
 import { Component } from "@domain/Component";
 import { Button, TextInput } from "@gravity-ui/uikit";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
+import styles from './styles.module.css'
 
 interface Props {
     componentsList: Component[];
@@ -36,43 +37,47 @@ export default function TechCardEdit({ componentsList, techCard, onSave }: Props
         })
     }, [])
 
-    const componentChangeHandler = useCallback((prevId: Component["id"], component: TechCard["components"][0]) => {
+    const componentChangeHandler = useCallback((index: number, component: TechCard["components"][0]) => {
 
         const { components } = techCardForm;
 
-        const index = components.findIndex(c => c.id === prevId);
-        if (index >= 0) {
-            const newComps = components.map((c, i) => {
-                if (i === index) {
-                    return (component)
-                }
-                return c;
-            })
-            setTechCardForm(prev => ({ ...prev, components: newComps }))
-        }
+        components[index] = component;
+
+        setTechCardForm(prev => ({ ...prev, components: [...components] }))
+
+    }, [techCardForm])
+
+    const newComponentAddBtnDisabled = useMemo(() => {
+        const { components } = techCardForm;
+        return components.length > 0 && components[components.length - 1].component.id === null
     }, [techCardForm])
 
     return (
-        <div>
-            {/* <pre>{JSON.stringify(techCardForm, null, 2)}</pre> */}
-            <TextInput
-                size='xl'
-                placeholder='Наименование'
-                value={techCardForm ? techCardForm.name : undefined}
-                onChange={(e) => setTechCardForm((prev) => ({ ...prev, name: e.target.value }))} />
-            <TextInput
-                size='xl'
-                placeholder='Код'
-                value={techCardForm ? techCardForm.code : undefined}
-                onChange={(e) => setTechCardForm((prev) => ({ ...prev, code: e.target.value }))} />
-            {techCardForm.components.map(c =>
-                <ComponentSelect key={c.id}
-                    componentList={componentsList}
-                    selectedComponent={c}
-                    onChange={componentChangeHandler}
-                />)}
-            <Button onClick={handleAddComponent}>Добавить компонент</Button>
-            <Button onClick={() => onSave && onSave(techCardForm)}>Сохранить</Button>
+        <div className={styles.editor}>
+            <div className={styles.editorForm}>
+                <TextInput
+                    size='xl'
+                    placeholder='Наименование'
+                    value={techCardForm ? techCardForm.name : undefined}
+                    onChange={(e) => setTechCardForm((prev) => ({ ...prev, name: e.target.value }))} />
+                <TextInput
+                    size='xl'
+                    placeholder='Код'
+                    value={techCardForm ? techCardForm.code : undefined}
+                    onChange={(e) => setTechCardForm((prev) => ({ ...prev, code: e.target.value }))} />
+            </div>
+            <div className={styles.editorComponents}>
+                {techCardForm.components.map((c, i) =>
+                    <ComponentSelect key={i}
+                        componentList={componentsList}
+                        selectedComponent={c}
+                        onChange={(component) => componentChangeHandler(i, component)}
+                    />)}
+            </div>
+            <div className={styles.editorActions}>
+                <Button onClick={handleAddComponent} disabled={newComponentAddBtnDisabled}>Добавить компонент</Button>
+                <Button onClick={() => onSave && onSave(techCardForm)}>Сохранить</Button>
+            </div>
         </div>
     )
 }
