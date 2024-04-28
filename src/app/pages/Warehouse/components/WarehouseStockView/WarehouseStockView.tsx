d3aa26f73d073e6faf80d99e8a9297b439add7ee
InspectorPilpsky <BoyarkoNV@gmail.com>
2024-04-28 16:34:56 +0300

    NNT-17 - Добавил пагинацию поставок/отгрузок на странице склада

--- src/app/pages/Warehouse/components/WarehouseStockView/WarehouseStockView.tsx
+++ src/app/pages/Warehouse/components/WarehouseStockView/WarehouseStockView.tsx
@@ -1,22 +1,36 @@
-import { WarehouseStock } from "@domain/Warehouse/WarehouseStock";
+import { WarehouseStock, WarehouseStockType } from "@domain/Warehouse/WarehouseStock";
 import { Table } from "@gravity-ui/uikit";
 
 interface Props {
     stockData: WarehouseStock[]
 }
+const stockTemplate = (stock: WarehouseStock, field: "name" | "code" | "category") => {
+    switch (field) {
+        case "name":
+            if (stock.type === WarehouseStockType.COMPONENT) return stock.component.name
+            else return stock.techCard.name
+        case "code":
+            if (stock.type === WarehouseStockType.COMPONENT) return stock.component.code
+            else return stock.techCard.code
+        case "category":
+            if (stock.type === WarehouseStockType.COMPONENT) return stock.component.category.name
+            else return null;
+        default:
+            return ""
+    }
+}
 
-export default function WarehouseStockView({stockData}: Props) {
-
+export default function WarehouseStockView({ stockData }: Props) {
     return (
         <Table
             data={stockData}
             columns={[
-                {id: "name", name: "Наименование", template: (data) => data.component.name},
-                {id: "code", name: "Код", template: (data) => data.component.code},
-                {id: "type", name: "Вид"},
-                {id: "category", name: "Категория", template: (data) => data.component.category.name},
-                {id: "amount", name: "Кол- во"},
-                {id: "cost", name: "Себестоимость"},
+                { id: "name", name: "Наименование", template: (data) => stockTemplate(data, "name") },
+                { id: "code", name: "Код", template: (data) => stockTemplate(data, "code") },
+                { id: "type", name: "Вид" },
+                { id: "category", name: "Категория", template: (data) => stockTemplate(data, "category") },
+                { id: "amount", name: "Кол- во" },
+                { id: "cost", name: "Себестоимость" },
             ]}
 
         />
--- src/domain/Warehouse/WarehouseStock.ts
+++ src/domain/Warehouse/WarehouseStock.ts
@@ -1,17 +1,33 @@
 import { Component, DEFAULT_COMPONENT } from "@domain/Component"
+import { TechCard } from ".."
 
-export type WarehouseStock = {
+export enum WarehouseStockType {
+    COMPONENT = "COMPONENT",
+    CARD = "CARD"
+}
+
+interface AbstractWarehouseStock {
     amount: number,
     cost: number,
-    type: string,
-    childId: number,
+    childId: number
+}
+
+export interface WarehouseCardStock extends AbstractWarehouseStock {
+    type: WarehouseStockType.CARD,
+    techCard: TechCard
+}
+
+export interface WarehouseComponentStock extends AbstractWarehouseStock {
+    type: WarehouseStockType.COMPONENT,
     component: Component
 }
+
+export type WarehouseStock = WarehouseCardStock | WarehouseComponentStock;
 
 export const DEFAULT_WAREHOUSE_STOCK: WarehouseStock = {
     amount: 0,
     cost: 0,
-    type: "",
+    type: WarehouseStockType.COMPONENT,
     childId: 0,
     component: DEFAULT_COMPONENT
 }
